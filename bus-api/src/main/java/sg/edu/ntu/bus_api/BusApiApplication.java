11b94f942d719b1b09a78b67eecabdb8fb0c2fd3
agentjimlam <jlam.5883@gmail.com>
2024-12-08 01:09:46 +0800

    first commit, created bus-api Springboot REST API, added base folders and updated pom.xml and application.properties

---
+++ bus-api/src/main/java/sg/edu/ntu/bus_api/BusApiApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.bus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApiApplication.class, args);
+	}
+
+}
---
+++ bus-api/src/test/java/sg/edu/ntu/bus_api/BusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.bus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
