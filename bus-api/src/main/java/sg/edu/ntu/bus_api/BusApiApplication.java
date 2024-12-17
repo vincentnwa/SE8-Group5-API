a792966bf4be756c77b4fac935415e0494f55c02
agentjimlam <jlam.5883@gmail.com>
2024-12-18 04:30:44 +0800

    added logging config, did a simple logger.info in the BusApiApplication.java's psvm. Added simple logger.error message in General exception handler

--- bus-api/src/main/java/sg/edu/ntu/bus_api/BusApiApplication.java
+++ bus-api/src/main/java/sg/edu/ntu/bus_api/BusApiApplication.java
@@ -1,12 +1,16 @@
 package sg.edu.ntu.bus_api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(BusApiApplication.class);
 	public static void main(String[] args) {
+		logger.info("🟡🟡🟡 Starting BusApiApplication");
 		SpringApplication.run(BusApiApplication.class, args);
 	}
 
