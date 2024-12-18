c13bcc8603ceca9db9b6dcfc3db3a3ad11a3a55e
agentjimlam <jlam.5883@gmail.com>
2024-12-19 00:03:23 +0800

    added MainBusStopController.java, MainBusStopService.java, MainBusStopServiceImpl.java. Removed extra impot in my ApiTestController.java

---
+++ bus-api/src/main/java/sg/edu/ntu/bus_api/controller/MainBusStopController.java
@@ -0,0 +1,71 @@
+package sg.edu.ntu.bus_api.controller;
+
+import sg.edu.ntu.bus_api.entity.BusStop;
+import sg.edu.ntu.bus_api.service.MainBusStopService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/app1")
+public class MainBusStopController {
+
+    private MainBusStopService mainBusStopService;
+
+    // Constructor injection
+    public MainBusStopController (MainBusStopService mainBusStopService){
+        this.mainBusStopService = mainBusStopService;
+    }
+    
+    // Read - get all Bus Stop
+    @GetMapping("/bus-stops-all")
+    public ResponseEntity<List<BusStop>> getAllBusStops() {
+        List<BusStop> allBusStops = mainBusStopService.getAllBusStops();
+        return new ResponseEntity<>(allBusStops, HttpStatus.OK);
+    }
+    
+    // Read - get one Bus Stop
+    @GetMapping("/bus-stops/{id}")
+    public ResponseEntity<BusStop> getBusStop(@PathVariable Long id) {
+        return new ResponseEntity<>(mainBusStopService.getBusStop(id), HttpStatus.OK);
+    }
+
+    // Create a bus stop. CAUTION: this method creates bus stop in the local database and it is persists
+    @PostMapping("/bus-stops")
+    public ResponseEntity<BusStop> createBusStop(@RequestBody BusStop busStop){
+        BusStop newBusStop = mainBusStopService.createBusStop(busStop);
+        return new ResponseEntity<>(newBusStop, HttpStatus.CREATED); 
+    }
+
+    // Delete
+    @DeleteMapping("/bus-stops/{id}")
+    public ResponseEntity<HttpStatus> deleteBusStop(@PathVariable Long id) {
+        mainBusStopService.deleteBusStop(id);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    // Update
+    @PutMapping("/bus-stops/{id}")
+    public ResponseEntity<BusStop> updateBusStop(@PathVariable Long id, @RequestBody BusStop busStop) {
+        BusStop updatedBusStop = mainBusStopService.updateBusStop(id, busStop);
+        return new ResponseEntity<>(updatedBusStop, HttpStatus.OK);
+    }
+
+}
+
+// When you annotate a class with @RestController, it becomes the entry point for all web requests. This means that any object you return from this class will be serialized i.e., converted to JSON and returned to the client.
+
+// @Controller -> Returns views (HTML, Thymeleaf templates)
+// @RestController -> Returns data (like JSON) for REST APIs
+// @RequestBody converts the received JSON to object during GET requests
---
+++ bus-api/src/main/java/sg/edu/ntu/bus_api/service/MainBusStopService.java
@@ -0,0 +1,20 @@
+package sg.edu.ntu.bus_api.service;
+
+import java.util.List;
+
+import sg.edu.ntu.bus_api.entity.BusStop;
+
+public interface MainBusStopService {
+    BusStop createBusStop(BusStop busStop);
+    BusStop getBusStop(Long id);
+    List<BusStop> getAllBusStops();
+
+    void deleteBusStop(Long id);
+    BusStop updateBusStop(Long id, BusStop busStop);
+
+}
+
+// An interface is like contract, anything that implements this has to have the below methods
+// it also acts like a middle-man device that when you switch to an improved CustomerServiceImpl code like switching App version, 
+// you can continue to refer to CustomerService in the Controller.java, and Spring boot will detect and find the correct version 
+// (but must set that improved CustomerServiceImpl.java as a bean and also @Primary if there are multiple versions implementing same interface, for Springboot to find the correct version to inject)
