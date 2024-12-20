5c2a5d6dcffd3caaf029bd790e62c5eac7918676
vincentnwa <vincentnwa@me.com>
2024-12-20 20:04:02 +0800

    Removed unused dependencies

--- bus-api/src/main/java/sg/edu/ntu/bus_api/entity/UserAccess.java
+++ bus-api/src/main/java/sg/edu/ntu/bus_api/entity/UserAccess.java
@@ -1,7 +1,6 @@
 package sg.edu.ntu.bus_api.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
